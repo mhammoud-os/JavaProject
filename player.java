670541309374b31b7ef5e8a68a5f7919ff13749d
Malek H <119138451+mhammoud-os@users.noreply.github.com>
2024-01-18 09:29:02 -0500

    Update player.java
    Added Bottom detection

--- player.java
+++ player.java
@@ -9,6 +9,9 @@ public class Player extends Rectangle{
 	int speed;
 	int gravity = 10;
 	boolean fall = true; 
+	boolean jumping = false;
+	int count = 0;
+	
 	Player(int speed, int width, int height){
 		this.speed =speed;
 		this.width = width; 
@@ -28,23 +31,38 @@ public class Player extends Rectangle{
 	void fall() {
 		if(fall) {
 			this.y+=gravity;
+			this.count ++;
+		}
+		System.out.println(count);
+	}
+	
+	void jump() {	
+		this.jumping = true;
+		if(this.jumping) {
+			this.y-=gravity+10;
 		}
 	}
-	void jump() {
-		this.y-=gravity+10;
-	}
 	void jumpdown() {
+		this.jumping = false;
 		//this.y -= gravity; 
 		
 	}
 	
-	void DetectPlatform(Rectangle rect) {
+	void DetectPlatform(Platform platform) {
 		//if (this.y > rect.y && this.y < rect.y+rect.height && this.x > rect.x && this.x < rect.x+rect.width) {
-		if (this.intersects(rect)) {
+		
+		if (this.intersects(platform.getTop())) {
 			this.fall=false;
-			this.y = rect.y-this.width;
+			this.y = platform.getTop().y-this.height;
 		}else this.fall = true;
+
+		if (this.intersects(platform.getBottom())) {
+			this.fall=true;
+			this.jumping = false;
+			this.y = platform.getBottom().y+(platform.height);
+			System.out.println("HI");
+		}
+		
 	}
-	void detectKeys() {
-	}
+	
 }
