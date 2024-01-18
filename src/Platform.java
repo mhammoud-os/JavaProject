3d877895d221ca462f70ab2234f18196459cd596
Malek H <119138451+mhammoud-os@users.noreply.github.com>
2024-01-18 09:30:07 -0500

    Update Platform.java

--- src/Platform.java
+++ src/Platform.java
@@ -1,19 +1,20 @@
+package graphics;
+
 import java.awt.Rectangle;
 
 public class Platform extends Rectangle {
-    Rectangle rectTop = new Rectangle(this.x, this.y, this.width, (int)this.height/2);
-    Rectangle rectBottom= new Rectangle(this.x, this.y+this.height/2, this.width, (int)this.height/2);
     Platform(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
     }
+    
     Rectangle getTop() {
-        return this.rectTop;
+    	return new Rectangle(this.x, this.y, this.width, (int)this.height/4);
     }
     Rectangle getBottom() {
-        return this.rectBottom;
+    	return new Rectangle(this.x, this.y+(this.height/4)*3, this.width, (int)this.height/4);
     }
 
 }
