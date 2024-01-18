43a9ad066d5865f2c4169a8f495220876488dc24
prabhxyz <prabhnannu07@gmail.com>
2024-01-18 09:26:01 -0500

    Use 2d Array

--- src/CreatePlatform.java
+++ src/CreatePlatform.java
@@ -6,28 +6,39 @@ public class CreatePlatform {
     GraphicsConsole gc = new GraphicsConsole(1280, 720);
     Random rand = new Random();
 
+    static int[][] blocks;
+
     public static void main(String[] args) {
         new CreatePlatform();
     }
 
     void setup() {
+        gc.setAntiAlias(true);
         gc.setBackgroundColor(Color.WHITE);
+        gc.setLocationRelativeTo(null);
         gc.clear();
     }
 
     CreatePlatform() {
         setup();
 
+        //Draw grass
         Platform mainPlatform = new Platform(0, 720 - 100, 1280, 100);
         gc.setColor(Color.GREEN);
         gc.fillRect(mainPlatform.x, mainPlatform.y, mainPlatform.width, mainPlatform.height);
 
+        createBlocks();
+        drawBlocks();
+
+        /*
         int[] blocks = new int[200];
         for (int i = 0; i < 200; i++) {
             blocks[i] = rand.nextInt(0, 2);
             System.out.println(blocks[i]);
         }
+        */
 
+        /*
         for (int i = 0; i < 200; i++) {
             if (blocks[i] == 1) {
                 gc.setColor(Color.BLACK);
@@ -42,7 +53,42 @@ public class CreatePlatform {
                 System.out.println(rectWidth);
                 System.out.println(rectHeight);
 
+                gc.setStroke(6);
+                gc.setColor(Color.BLACK);
                 gc.fillRect(rectX, rectY, rectWidth, rectHeight);
+                gc.drawRect(rectX, rectY, rectWidth, rectHeight);
+            }
+        }
+        */
+
+
+    }
+
+    void createBlocks() {
+        blocks = new int[10][35];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 35; j++) {
+                blocks[i][j] = rand.nextInt(0, 2);
+            }
+        }
+    }
+
+    void drawBlocks() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 35; j++) {
+                if (blocks[i][j] == 1 && i % 2 == 1) {
+                    gc.setColor(Color.BLACK);
+
+                    int rectX = 100 + (31 * (j));
+                    int rectY = 100 + (48 * (i));
+                    int rectWidth = 30;
+                    int rectHeight = 30;
+
+                    gc.setStroke(6);
+                    gc.setColor(Color.BLACK);
+                    gc.fillRect(rectX, rectY, rectWidth+30, rectHeight);
+                    gc.drawRect(rectX, rectY, rectWidth+30, rectHeight);
+                }
             }
         }
     }
--- src/Platform.java
+++ src/Platform.java
@@ -1,10 +1,19 @@
 import java.awt.Rectangle;
 
 public class Platform extends Rectangle {
+    Rectangle rectTop = new Rectangle(this.x, this.y, this.width, (int)this.height/2);
+    Rectangle rectBottom= new Rectangle(this.x, this.y+this.height/2, this.width, (int)this.height/2);
     Platform(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
     }
+    Rectangle getTop() {
+        return this.rectTop;
+    }
+    Rectangle getBottom() {
+        return this.rectBottom;
+    }
+
 }
