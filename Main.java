6ad80812e00e3b134b2b0f7930ddbb1bafff1060
prabhxyz <prabhnannu07@gmail.com>
2024-01-22 15:28:02 -0500

    Add Direction to Sprites

--- Main.java
+++ Main.java
@@ -23,9 +23,12 @@ public class Main implements ActionListener {
 	static int[][] blocks;
 	boolean AutomaticGame = true;
 	static BufferedImage BackgroundImg, BlockImg, PlatformImg;
-	static BufferedImage[] pImages = new BufferedImage[12];
-	static BufferedImage[] bImages = new BufferedImage[12];
+	static BufferedImage[] pImagesRight = new BufferedImage[12];
+	static BufferedImage[] pImagesLeft = new BufferedImage[12];
+	static BufferedImage[] bImagesRight = new BufferedImage[12];
+	static BufferedImage[] bImagesLeft = new BufferedImage[12];
 	int currentFrame = 0;
+	boolean PlayerMoveRight = true;
 
 	public static void main(String[] args) {
 		new Main();
@@ -51,8 +54,10 @@ public class Main implements ActionListener {
 		PlatformImg = loadImage("src/imgs/groundPlatformImg.png");
 
 		for (int i = 0; i < 12; i++) {
-			pImages[i] = loadImage("src/imgs/player/0_Minotaur_Running_0" + String.format("%02d", i) + ".png");
-			bImages[i] = loadImage("src/imgs/player2/0_Reaper_Man_Run Slashing_0" + String.format("%02d", i) + ".png");
+			pImagesLeft[i] = loadImage("src/imgs/player/right/0_Minotaur_Running_0" + String.format("%02d", i) + ".png");
+			bImagesRight[i] = loadImage("src/imgs/player2/right/0_Reaper_Man_Run Slashing_0" + String.format("%02d", i) + ".png");
+			pImagesRight[i] = loadImage("src/imgs/player/left/0_Minotaur_Running_0" + String.format("%02d", i) + ".png");
+			bImagesLeft[i] = loadImage("src/imgs/player2/left/0_Reaper_Man_Run Slashing_0" + String.format("%02d", i) + ".png");
 		}
 
 		gc.setLocationRelativeTo(null);
@@ -91,12 +96,8 @@ public class Main implements ActionListener {
 					gc.drawRect(rectX, rectY, rectWidth + 30, rectHeight);
 					gc.drawImage(BlockImg, rectX, rectY, rectWidth + 30, rectHeight);
 					Platform platform = new Platform(rectX, rectY, rectWidth + 30, rectHeight);
-					if (player.fall) {
-						player.DetectPlatform(platform);
-					}
-					if (player2.fall) {
-						player2.DetectPlatform(platform);
-					}
+					if (player.fall) player.DetectPlatform(platform);
+					if (player2.fall) player2.DetectPlatform(platform);
 				}
 			}
 		}
@@ -108,20 +109,12 @@ public class Main implements ActionListener {
 			gc.drawImage(BackgroundImg, 0, 0, 1280, 720);
 
 			player.DetectPlatform(leftSide);
-			if (player.fall) {
-				player.DetectPlatform(rightSide);
-			}
-			if (player.fall) {
-				player.DetectPlatform(mainPlatform);
-			}
+			if (player.fall) player.DetectPlatform(rightSide);
+			if (player.fall) player.DetectPlatform(mainPlatform);
 
 			player2.DetectPlatform(leftSide);
-			if (player2.fall) {
-				player2.DetectPlatform(rightSide);
-			}
-			if (player2.fall) {
-				player2.DetectPlatform(mainPlatform);
-			}
+			if (player2.fall) player2.DetectPlatform(rightSide);
+			if (player2.fall) player2.DetectPlatform(mainPlatform);
 
 			gc.setColor(Color.GREEN);
 			gc.fillRect(mainPlatform.x, mainPlatform.y, mainPlatform.width, mainPlatform.height);
@@ -131,9 +124,15 @@ public class Main implements ActionListener {
 			gc.setColor(new Color(100, 100, 100, 0));
 			gc.fillRect(player.x, player.y, player.width, player.height);
 
-			gc.drawImage(pImages[currentFrame], player.x - 40, player.y - 55, player.width + 80, player.height + 80);
+			if (PlayerMoveRight)
+				gc.drawImage(pImagesRight[currentFrame], player.x - 40, player.y - 55, player.width + 80, player.height + 80);
+			else
+				gc.drawImage(pImagesLeft[currentFrame], player.x - 40, player.y - 55, player.width + 80, player.height + 80);
 
-			gc.drawImage(bImages[currentFrame], player2.x - 40, player2.y - 55, player2.width + 80, player2.height + 80);
+			if (player2.x > player.x)
+				gc.drawImage(bImagesLeft[currentFrame], player2.x - 40, player2.y - 55, player2.width + 80, player2.height + 80);
+			else
+				gc.drawImage(bImagesRight[currentFrame], player2.x - 40, player2.y - 55, player2.width + 80, player2.height + 80);
 
 			currentFrame = (currentFrame + 1) % 12;
 
@@ -155,74 +154,79 @@ public class Main implements ActionListener {
 		int constantMoveSpeed = 1;
 
 		if (Math.abs(distanceX) > Math.abs(distanceY)) {
-			if (distanceX > 0) {
+			if (distanceX > 0)
 				player2.moveRight(moveSpeed);
-			} else {
+			else
 				player2.moveLeft(moveSpeed);
-			}
 		} else {
-			if (distanceY > 0) {
+			if (distanceY > 0)
 				player2.fall = true;
-			} else {
-				if (!player2.jumping && player.y - player2.y > 30 && Math.abs(distanceX) > 10) {
-					player2.jumping = true;
-					player2.gravity = -15;
-				}
+			else if (!player2.jumping && player.y - player2.y > 30 && Math.abs(distanceX) > 10) {
+				player2.jumping = true;
+				player2.gravity = -15;
 			}
 		}
 
 		if (!player2.jumping) {
-			if (distanceX > 50) {
+			if (distanceX > 50)
 				player2.moveRight(constantMoveSpeed);
-			} else if (distanceX < -50) {
+			else if (distanceX < -50)
 				player2.moveLeft(constantMoveSpeed);
-			}
 		}
 	}
 
 	void detectKeys() {
-		if (AutomaticGame) {
+		if (AutomaticGame)
 			updatePlayer2Position();
+
+		if (gc.isKeyDown(37)) {
+			player.moveLeft();
+			PlayerMoveRight = false;
 		}
-		if (gc.isKeyDown(37)) player.moveLeft();
-		if (gc.isKeyDown(39)) player.moveRight();
+		if (gc.isKeyDown(39)) {
+			player.moveRight();
+			PlayerMoveRight = true;
+		}
 		if (gc.isKeyDown(38) && !timer.isRunning() && !player.fall) {
 			timer.start();
 			player.jumping = true;
 		}
-		if (gc.isKeyDown(40)) player.jumpdown();
+		if (gc.isKeyDown(40))
+			player.jumpdown();
 
-		if (gc.isKeyDown(65)) player2.moveLeft();
-		if (gc.isKeyDown(68)) player2.moveRight();
+		if (gc.isKeyDown(65))
+			player2.moveLeft();
+		if (gc.isKeyDown(68))
+			player2.moveRight();
 		if (!timer2.isRunning() && !player2.fall) {
 			timer2.start();
 			player2.jumping = true;
 		}
-		if (gc.isKeyDown(83)) player2.jumpdown();
+		if (gc.isKeyDown(83))
+			player2.jumpdown();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent ev) {
-		if (ev.getSource() == timer2) {
+		if (ev.getSource() == timer2)
 			time2 += 0.1;
-		}
-		if (ev.getSource() == timer) {
+		if (ev.getSource() == timer)
 			time += 0.1;
-		}
 
-		if (time2 >= 3) player2.jumping = false;
+		if (time2 >= 3)
+			player2.jumping = false;
 		if (time2 >= 6) {
 			time2 = 0;
 			timer2.stop();
 		}
 
-		if (time >= 3) player.jumping = false;
+		if (time >= 3)
+			player.jumping = false;
 		if (time >= 6) {
 			time = 0;
 			timer.stop();
 		}
 	}
-
 
 	static BufferedImage loadImage(String fileName) {
 		BufferedImage img = null;
