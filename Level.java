3f20330797ddc72bd8e3f8d0b90e6296a0219731
Milo <156622106+SaberDragon333@users.noreply.github.com>
2024-01-22 08:29:23 -0500

    Create Level.java

---
+++ Level.java
@@ -0,0 +1,21 @@
+import java.awt.Color;
+
+public class Level{
+	Color levelBg;
+	String levelName;
+	
+	Level(int l) {
+		
+		
+		if (l%3 == 0) {
+			levelName = "Easy";
+			levelBg = Color.PINK;
+		} else if(l%3 == 1) {
+			levelName = "Medium";
+			levelBg = Color.ORANGE;
+		} else if(l%3 == 2) {
+			levelName = "Hard";
+			levelBg = Color.RED;
+		}
+	}
+}
